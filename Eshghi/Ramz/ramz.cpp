14eb46857034e200e1a1ee689a621f3a734d92bc
soreana <soreana@users.noreply.github.com>
2014-11-29 08:20:16 -0800

    Create ramz.cpp

---
+++ Eshghi/Ramz/ramz.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define n 1000
+int main(){
+	string vo;
+	cin>>vo;
+for(int i=0;i<n;i++){
+	if(vo[i]=='a'||vo[i]=='A')
+		cout<<"2";
+	if(vo[i]=='b'||vo[i]=='B')
+		cout<<"22";
+	if(vo[i]=='c'||vo[i]=='C')
+		cout<<"222";
+	if(vo[i]=='d'||vo[i]=='D')
+		cout<<"3";
+	if(vo[i]=='e'||vo[i]=='E')
+		cout<<"33";
+	if(vo[i]=='f'||vo[i]=='F')
+		cout<<"333";
+	if(vo[i]=='g'||vo[i]=='G')
+		cout<<"4";
+	if(vo[i]=='h'||vo[i]=='H')
+		cout<<"44";
+	if(vo[i]=='i'||vo[i]=='I')
+		cout<<"444";
+	if(vo[i]=='j'||vo[i]=='J')
+		cout<<"5";
+	if(vo[i]=='k'||vo[i]=='K')
+		cout<<"55";
+	if(vo[i]=='l'||vo[i]=='L')
+		cout<<"555";
+	if(vo[i]=='m'||vo[i]=='M')
+		cout<<"6";
+	if(vo[i]=='n'||vo[i]=='N')
+		cout<<"66";
+	if(vo[i]=='o'||vo[i]=='O')
+		cout<<"666";
+	if(vo[i]=='p'||vo[i]=='P')
+		cout<<"7";
+	if(vo[i]=='q'||vo[i]=='Q')
+		cout<<"77";
+	if(vo[i]=='r'||vo[i]=='R')
+		cout<<"777";
+	if(vo[i]=='s'||vo[i]=='S')
+		cout<<"7777";
+	if(vo[i]=='t'||vo[i]=='T')
+		cout<<"8";
+	if(vo[i]=='u'||vo[i]=='U')
+		cout<<"88";
+	if(vo[i]=='v'||vo[i]=='V')
+		cout<<"888";
+	if(vo[i]=='w'||vo[i]=='W')
+		cout<<"9";
+	if(vo[i]=='x'||vo[i]=='X')
+		cout<<"99";
+	if(vo[i]=='y'||vo[i]=='Y')
+		cout<<"999";
+	if(vo[i]=='z'||vo[i]=='Z')
+		cout<<"9999";
+}//end of the for
+cout<<endl;
+}//end of the main
